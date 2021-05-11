ca27661f87f72d7d89dabc13332eaa387cff87fc
Ken Ng <kenng@itwonders-web.com>
2021-05-11 08:43:10 +0800

    optimise bubble sort

--- src/algorithms/BubbleSort.ts
+++ src/algorithms/BubbleSort.ts
@@ -2,6 +2,8 @@ import { IData } from 'src/data/index';
 import { SortBase } from './SortBase';
 
 export class BubbleSort extends SortBase {
+    private hasSwapped!: boolean;
+
     constructor(rawData: IData[], nextIndex = 0) {
         super(rawData, nextIndex);
     }
@@ -13,11 +15,11 @@ export class BubbleSort extends SortBase {
             this.data[this.currentIndex].value >
             this.data[this.currentIndex + 1].value
         ) {
-            // swap
             [this.data[this.currentIndex], this.data[this.currentIndex + 1]] = [
                 this.data[this.currentIndex + 1],
                 this.data[this.currentIndex],
             ];
+            this.hasSwapped = true;
         }
 
         return this.data;
@@ -30,6 +32,7 @@ export class BubbleSort extends SortBase {
             this.nextIndex++
         ) {
             const lastUnsortedIndex = this.data.length - this.nextIndex - 1;
+            this.hasSwapped = false;
             for (
                 this.currentIndex = 0;
                 this.currentIndex < lastUnsortedIndex;
@@ -37,7 +40,14 @@ export class BubbleSort extends SortBase {
             ) {
                 yield this.sortNext();
             }
-            this.data[this.currentIndex].sorted = true;
+
+            if (!this.hasSwapped) {
+                this.data.map((item) => (item.sorted = true));
+                break;
+            } else {
+                // last element is sorted
+                this.data[this.currentIndex].sorted = true;
+            }
         }
     }
 }
